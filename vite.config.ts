4a57bf2054d19a5dad325f216916cf1032cd141c
wheellessds <wheellessd@gmail.com>
2026-02-03 21:28:45 +0800

    修正 Vite 網址存取權限

--- vite.config.ts
+++ vite.config.ts
@@ -119,6 +119,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: '0.0.0.0',
+      allowedHosts: true,
     },
     plugins: [react(), leaderboardStoragePlugin()],
     define: {
